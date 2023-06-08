1bbdcf1d2d524f9d82cb952e3d4e4aa5769030de
kobashi_hayato <k021g1128@g.neec.ac.jp>
2023-06-08 12:51:21 +0900

    3-2まで完成

--- DirectXGame/2d/PostEffect.h
+++ DirectXGame/2d/PostEffect.h
@@ -1,14 +1,39 @@
 #pragma once
+
 #include "Sprite.h"
+
+
+
 class PostEffect :
     public Sprite
 {
+
+protected://エイリアス
+    //Microsoft::WRL::を省略
+    template<class T>using ComPtr = Microsoft::WRL::ComPtr<T>;
+    //DirectX::を省略
+    using XMFLOAT2 = DirectX::XMFLOAT2;
+    using XMFLOAT3 = DirectX::XMFLOAT3;
+    using XMFLOAT4 = DirectX::XMFLOAT4;
+    using XMMATRIX = DirectX::XMMATRIX;
+
+public:
+    //テクスチャバッファ
+    ComPtr<ID3D12Resource>texBuff;
+    //SRV用デスクリプタヒープ
+    ComPtr<ID3D12DescriptorHeap>descHeapSRV;
+
 public:
     /// <summary>
     /// コンストラクタ
     /// </summary>
     PostEffect();
     
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    void Initialize();
+
     /// <summary>
     /// 描画コマンドの発行
     /// </summary>
--- DirectXGame/PostEffect.cpp
+++ DirectXGame/PostEffect.cpp
@@ -1,4 +1,5 @@
 #include "PostEffect.h"
+#include "WinApp.h"
 #include <d3dx12.h>
 
 using namespace DirectX;
@@ -6,13 +7,79 @@ using namespace DirectX;
 PostEffect::PostEffect()
 	:Sprite(
 		100,//テクスチャ
-		{0,0},//座標
-		{500.0f,500.0f},//サイズ
-		{1,1,1,1},//色
-		{0.0f,0.0f},//アンカーポイント
+		{ 0,0 },//座標
+		{ 500.0f,500.0f },//サイズ
+		{ 1,1,1,1 },//色
+		{ 0.0f,0.0f },//アンカーポイント
 		false,//左右反転フラグ
 		false)//上下反転フラグ
 {
+}
+
+void PostEffect::Initialize()
+{
+	HRESULT result;
+
+	//基礎クラスとして初期化
+	Sprite::Initialize();
+	//テクスチャリソース設定
+	CD3DX12_RESOURCE_DESC texresDesc = CD3DX12_RESOURCE_DESC::Tex2D(
+		DXGI_FORMAT_R8G8B8A8_UNORM_SRGB,
+		WinApp::window_width,
+		(UINT)WinApp::window_height,
+		1, 0, 1, 0, D3D12_RESOURCE_FLAG_ALLOW_RENDER_TARGET
+	);
+
+	//テクスチャバッファの生成
+	result = device->CreateCommittedResource(
+		&CD3DX12_HEAP_PROPERTIES(D3D12_CPU_PAGE_PROPERTY_WRITE_BACK,
+			D3D12_MEMORY_POOL_L0),
+		D3D12_HEAP_FLAG_NONE,
+		&texresDesc,
+		D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE,
+		nullptr,
+		IID_PPV_ARGS(&texBuff));
+	assert(SUCCEEDED(result));
+
+	{
+		//画素数
+		const UINT pixelCount = WinApp::window_width * WinApp::window_height;
+		//画像1行分のデータサイズ
+		const UINT rowPitch = sizeof(UINT) * WinApp::window_width;
+		//画像全体のデータサイズ
+		const UINT depthPitch = rowPitch * WinApp::window_height;
+		//画像イメージ
+		UINT* img = new UINT[pixelCount];
+		for (int i = 0; i < pixelCount; i++){img[i] = 0xff0000ff;}
+
+		//テクスチャバッファにデータ転送
+		result = texBuff->WriteToSubresource(0, nullptr,
+			img, rowPitch, depthPitch);
+		assert(SUCCEEDED(result));
+		delete[]img;
+	}
+
+	//SRV用デスクリプタヒープ
+	D3D12_DESCRIPTOR_HEAP_DESC srvDescHeapDesc = {};
+	srvDescHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
+	srvDescHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
+	srvDescHeapDesc.NumDescriptors = 1;
+	//SRV用デスクリプタヒープ
+	result = device->CreateDescriptorHeap(&srvDescHeapDesc, IID_PPV_ARGS(&descHeapSRV));
+	assert(SUCCEEDED(result));
+	//SRV作成
+	D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc{};
+	srvDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
+	srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+	srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
+	srvDesc.Texture2D.MipLevels = 1;
+
+	//デスクリプタヒープにSRVを生成
+	device->CreateShaderResourceView(texBuff.Get(),
+		&srvDesc,
+		descHeapSRV->GetCPUDescriptorHandleForHeapStart()
+	);
+
 }
 
 void PostEffect::Draw(ID3D12GraphicsCommandList* cmdList)
@@ -40,14 +107,14 @@ void PostEffect::Draw(ID3D12GraphicsCommandList* cmdList)
 	// 頂点バッファの設定
 	cmdList->IASetVertexBuffers(0, 1, &this->vbView);
 
-	ID3D12DescriptorHeap* ppHeaps[] = { descHeap.Get() };
+	ID3D12DescriptorHeap* ppHeaps[] = { descHeapSRV.Get() };
 	// デスクリプタヒープをセット
 	cmdList->SetDescriptorHeaps(_countof(ppHeaps), ppHeaps);
 	// 定数バッファビューをセット
 	cmdList->SetGraphicsRootConstantBufferView(0, this->constBuff->GetGPUVirtualAddress());
 	// シェーダリソースビューをセット
-	cmdList->SetGraphicsRootDescriptorTable(1, CD3DX12_GPU_DESCRIPTOR_HANDLE(descHeap->GetGPUDescriptorHandleForHeapStart(), this->texNumber, descriptorHandleIncrementSize));
-	// 描画コマンド
+	cmdList->SetGraphicsRootDescriptorTable(1, descHeap->GetGPUDescriptorHandleForHeapStart());
+	
 	cmdList->DrawInstanced(4, 1, 0, 0);
 
 }
