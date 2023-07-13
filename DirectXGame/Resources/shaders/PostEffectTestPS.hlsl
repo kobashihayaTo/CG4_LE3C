1473cc6b7c7bd1e714e61eb49a44807b4bc7676d
kobashi_hayato <k021g1128@g.neec.ac.jp>
2023-07-13 14:21:28 +0900

    評価完成

--- DirectXGame/Resources/shaders/PostEffectTestPS.hlsl
+++ DirectXGame/Resources/shaders/PostEffectTestPS.hlsl
@@ -8,13 +8,35 @@ float4 main(VSOutput input) : SV_TARGET
 {
 	float4 colortex0 = tex0.Sample(smp,input.uv);
 	float4 colortex1 = tex1.Sample(smp,input.uv);
+    float4 blur = 0.0f;
 
-	float4 color = colortex0;
-	if (fmod(input.uv.y, 0.1f) < 0.05f) {
-		color = colortex1;
-	}
+    //テクスチャのサイズを指定
+    int textureWidth = 512;  // テクスチャの幅
+    int textureHeight = 512; // テクスチャの高さ
+
+    // サンプリング範囲の設定
+    float2 texelSize = float2(1.0 / textureWidth, 1.0 / textureHeight);
+
+    // サンプリング範囲内のピクセルを合計
+    for (int x = -1; x <= 1; x++)
+    {
+        for (int y = -1; y <= 1; y++)
+        {
+           blur += tex1.Sample(smp, input.uv+float2(texelSize.x*x, texelSize.y*y));
+        }
+    }
+    colortex1 = blur / 9.0f;
+   
+
+    float4 color = 1 - colortex0;
+    if(fmod(input.uv.y,0.1f)<0.05f){
+
+        color = lerp(colortex0, colortex1, 0.4f);
+    }
+
 
 	return float4(color.rgb, 1);
+
 }
 
 //float4 main(VSOutput input) : SV_TARGET
